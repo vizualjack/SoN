c724e0c64a29c430c656dd62a14da50d9943e25c
vizualjack <67346906+vizualjack@users.noreply.github.com>
2022-06-19 20:32:29 +0200

    progress... start to run on android

--- forAndroid/src/main/java/son/android/MainActivity.java
+++ forAndroid/src/main/java/son/android/MainActivity.java
@@ -1,19 +1,125 @@
 package son.android;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
+import android.os.FileUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
-import son.Main;
+import com.obsez.android.lib.filechooser.ChooserDialog;
+
+import java.io.File;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int MY_REQUEST_CODE_PERMISSION = 1000;
+    private static final int MY_RESULT_CODE_FILECHOOSER = 2000;
+
+    Button btn;
+    TextView text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        final TextView textView = (TextView) findViewById(R.id.testText);
-        textView.setText(new Main().testVal);
+        text = (TextView) findViewById(R.id.testText);
+        btn = (Button) findViewById(R.id.button);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                askPermissionAndBrowseFile();
+            }
+        });
+    }
+
+    private String showFiles(File folder) {
+        String out = "";
+        for(String file : folder.list()) {
+            out += file + ",";
+        }
+        return out;
+    }
+
+    private void askPermissionAndBrowseFile()  {
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.M) {
+            int permisson = ActivityCompat.checkSelfPermission(getBaseContext(),
+                    Manifest.permission.READ_EXTERNAL_STORAGE);
+
+            if (permisson != PackageManager.PERMISSION_GRANTED) {
+                this.requestPermissions(
+                        new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                        MY_REQUEST_CODE_PERMISSION
+                );
+                return;
+            }
+        }
+        doBrowseFile();
+    }
+
+    private void doBrowseFile()  {
+        Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT_TREE);
+        startActivityForResult(intent, MY_RESULT_CODE_FILECHOOSER);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        //
+        switch (requestCode) {
+            case MY_REQUEST_CODE_PERMISSION: {
+
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    Toast.makeText(this.getBaseContext(), "Permission granted!", Toast.LENGTH_SHORT).show();
+
+                    doBrowseFile();
+                }
+
+                else {
+                    Toast.makeText(getBaseContext(), "Permission denied!", Toast.LENGTH_SHORT).show();
+                }
+                break;
+            }
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch (requestCode) {
+            case MY_RESULT_CODE_FILECHOOSER:
+                if (resultCode == Activity.RESULT_OK ) {
+                    if(data != null)  {
+                        Uri fileUri = data.getData();
+
+                        String filePath = null;
+                        /*try {
+                            filePath = FileUtils.getPath(getBaseContext(),fileUri);
+                        } catch (Exception e) {
+                            Log.e(LOG_TAG,"Error: " + e);
+                            Toast.makeText(getBaseContext(), "Error: " + e, Toast.LENGTH_SHORT).show();
+                        }*/
+                        text.setText(fileUri.getPath());
+                    }
+                }
+                break;
+        }
+        super.onActivityResult(requestCode, resultCode, data);
     }
 }
