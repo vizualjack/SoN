893b4c5cdb3d3652f1787c47e17a4d44aefa44d2
vizualjack <67346906+vizualjack@users.noreply.github.com>
2022-07-07 19:30:17 +0200

    test set sync folder with start param

--- forLinux/src/main/java/son/linux/App.java
+++ forLinux/src/main/java/son/linux/App.java
@@ -1,24 +1,32 @@
 package son.linux;
 
 import java.io.File;
+import java.io.IOException;
+
 import son.Syncer;
 
 public class App {
-    public static void main(String[] args) {
-        var f = new File("testFolder");
-        f.mkdir();
-        var syncer = new Syncer(f);
+    public static void main(String[] args) throws IOException {
+        var syncFolderPath = args[0];
+        System.out.println("Set syncFolder: " + syncFolderPath);
 
-        while(true) {
-            try {
-                Thread.sleep(10000);
-                System.out.println("Syncing...");
-                syncer.sync();
-            } catch (InterruptedException e) {
-                System.out.println("Thread can't sleep");
-            }
+        var f = new File(syncFolderPath);
+        new File(f, "helloImA new fiii1le").createNewFile();
+        
+        
+        // f.mkdir();
+        // var syncer = new Syncer(f);
 
-        }
+        // while(true) {
+        //     try {
+        //         Thread.sleep(10000);
+        //         System.out.println("Syncing...");
+        //         syncer.sync();
+        //     } catch (InterruptedException e) {
+        //         System.out.println("Thread can't sleep");
+        //     }
+
+        // }
         // syncer.startServer();
 
         // var clientHolder = new ClientHolder(1337);
