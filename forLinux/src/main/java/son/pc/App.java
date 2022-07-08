9da15c102b59f0c5602c0c2b01f1311b22080dc7
vizualjack <67346906+vizualjack@users.noreply.github.com>
2022-07-08 18:49:44 +0200

    app for pc

--- forLinux/src/main/java/son/pc/App.java
+++ forLinux/src/main/java/son/pc/App.java
@@ -3,32 +3,31 @@ package son.pc;
 import java.io.File;
 import java.io.IOException;
 
+import son.Syncer;
+
 public class App {
     public static void main(String[] args) throws IOException {
-        var syncFolderPath = args[0];
-        System.out.println("Set syncFolder: " + syncFolderPath);
+        File f = null;
+        if(args.length >= 1) {
+            f = new File(args[0]);
+            System.out.println("Set syncFolder: " + f.getPath());
+        }
+        else {
+            System.out.println("Create test folder...");
+            f = new File("testFolder");
+            f.mkdir();
+        }
+  
+        var syncer = new Syncer(new SyncFolderPC(f));
+        while(true) {
+            try {
+                Thread.sleep(10000);
+                System.out.println("Syncing...");
+                syncer.sync();
+            } catch (InterruptedException e) {
+                System.out.println("Thread can't sleep");
+            }
 
-        var f = new File(syncFolderPath);
-        new File(f, "helloImA new fiii1le").createNewFile();
-        
-        
-        // f.mkdir();
-        // var syncer = new Syncer(f);
-
-        // while(true) {
-        //     try {
-        //         Thread.sleep(10000);
-        //         System.out.println("Syncing...");
-        //         syncer.sync();
-        //     } catch (InterruptedException e) {
-        //         System.out.println("Thread can't sleep");
-        //     }
-
-        // }
-        // syncer.startServer();
-
-        // var clientHolder = new ClientHolder(1337);
-        // clientHolder.start();
-        // System.out.println("clientholder started");
+        }
     }
 }
